0fb5629760aa2104e82cf2b4ace0e13ed60fa5cb
Silvio Silva <smsilva@gmail.com>
2021-06-19 16:55:05 -0300

    Update

--- app/variables.tf
+++ app/variables.tf
@@ -6,10 +6,12 @@ variable location {
     default = "centralus"
 }
 
-variable description {
-    default = ""
+variable tag_description {
+    type        = string
+    description = "The id of the machine image (AMI) to use for the server."
+    
     validation {
-        condition     = length(var.description) > 0
+        condition     = can(length(var.tag_description) > 0)
         error_message = "The description must be informed."
     }
 }
