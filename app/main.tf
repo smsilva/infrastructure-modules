6f0de6ef283bb7cc746eec3b5ccecd2832e1b96c
Silvio Silva <smsilva@gmail.com>
2021-06-19 17:17:56 -0300

    Update

--- app/main.tf
+++ app/main.tf
@@ -2,6 +2,7 @@ resource "azurerm_resource_group" "default" {
     name     = var.name
     location = var.location
     tags     = {
+        environment = "${var.environment}"
         description = "${var.tag_description}"
     }
 }
--- app/variables.tf
+++ app/variables.tf
@@ -1,17 +1,28 @@
-variable name {
-    default = ""
+variable "name" {
+  default = ""
 }
 
-variable location {
-    default = "centralus"
+variable "location" {
+  default = ""
 }
 
-variable tag_description {
-    type        = string
-    description = "The id of the machine image (AMI) to use for the server."
-    
-    validation {
-        condition     = can(length(var.tag_description) > 0)
-        error_message = "The description must be informed."
-    }
+variable "tag_environment" {
+  type        = string
+  description = "The Infrastructure Environment like: sandbox, dev, qa, stage, prod"
+
+  validation {
+    condition     = length(var.tag_environment) > 0
+    error_message = "[tag] The environment must be informed."
+  }
+}
+
+
+variable "tag_description" {
+  type        = string
+  description = "The Resource Group Description."
+
+  validation {
+    condition     = length(var.tag_description) > 0
+    error_message = "[tag] The description must be informed."
+  }
 }
